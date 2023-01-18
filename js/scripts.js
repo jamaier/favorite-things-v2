427813005903ad86bbdb4be1cc98d9481b568257
Jacob Maier <jacobamaier@gmail.com>
2023-01-17 16:29:45 -0800

    add additional list items to array.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,16 @@ window.addEventListener("load", function() {
       event.preventDefault();
 
       const thing1 = document.getElementById('thing-1').value;
+      const thing2 = document.getElementById('thing-2').value;
+      const thing3 = document.getElementById('thing-3').value;
 
-      const myArray = [thing1];
+      const myArray = [thing1, thing2, thing3];
 
       const firstThing = myArray[0];
+      const secondThing = myArray[1];
+      const thirdThing = myArray[2];
 
-      const newArray = [firstThing];
+      const newArray = [firstThing, secondThing, thirdThing];
 
       const newList = document.getElementById("userList");
       for (let input = 0; input <newArray.length; input++) {
